536b80645d04770c6fa45f49d7b364eb507ada5e
Felipe Oliveira Rocha <felipeoliveira02114@gmail.com>
2024-09-22 12:30:06 -0300

    correcao das funcoes de transformacoes

--- transformacoes.c
+++ transformacoes.c
@@ -1,92 +1,90 @@
 #include "transformacoes.h"
 
-void transladar(GLfloat tx, GLfloat ty) {
-    GLfloat matriz_translacao[16] = {
-        1.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
-        tx, ty, 0.0, 1.0
-    };
+void multMatriz(GLfloat matriz[3][3], Ponto* pontos, int num_pontos) {
+    for (int i = 0; i < num_pontos; i++) {
+        GLfloat x = pontos[i].x;
+        GLfloat y = pontos[i].y;
+        GLfloat w = 1.0;
 
-    glMultMatrixf(matriz_translacao);
+        GLfloat novo_x = matriz[0][0] * x + matriz[0][1] * y + matriz[0][2] * w;
+        GLfloat novo_y = matriz[1][0] * x + matriz[1][1] * y + matriz[1][2] * w;
+
+        pontos[i].x = novo_x;
+        pontos[i].y = novo_y;
+    }
+}
+
+void transladar(GLfloat tx, GLfloat ty) {
+    GLfloat matriz_translacao[3][3] = {
+        {1.0, 0.0, tx},
+        {0.0, 1.0, ty},
+        {0.0, 0.0, 1.0}
+    };
+    multMatriz(matriz_translacao, triangulo, 3);
 }
 
 void rotacionar(GLfloat angulo) {
     GLfloat cose = cos(angulo * M_PI / 180.0);
     GLfloat sen = sin(angulo * M_PI / 180.0);
 
-    GLfloat matriz_rotacao[16] = {
-        cose, sen, 0.0, 0.0,
-        -sen, cose, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
-        0.0, 0.0, 0.0, 1.0
+    GLfloat matriz_rotacao[3][3] = {
+        {cose, -sen, 0.0},
+        {sen, cose, 0.0},
+        {0.0, 0.0, 1.0}
     };
-
-    glMultMatrixf(matriz_rotacao);
+    multMatriz(matriz_rotacao, triangulo, 3);
 }
 
-void escalar(GLfloat x, GLfloat y) {
-    GLfloat matriz_escala[16] = {
-        x, 0.0, 0.0, 0.0,
-        0.0, y, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
-        0.0, 0.0, 0.0, 1.0
+void escalar(GLfloat sx, GLfloat sy) {
+    GLfloat matriz_escalar[3][3] = {
+        {sx, 0.0, 0.0},
+        {0.0, sy, 0.0},
+        {0.0, 0.0, 1.0}
     };
-
-    glMultMatrixf(matriz_escala);
-}
-
-void reflexao_vertical() {
-    GLfloat matriz_reflexao[16] = {
-        1.0,  0.0, 0.0, 0.0,
-        0.0, -1.0, 0.0, 0.0,
-        0.0,  0.0, 1.0, 0.0,
-        0.0,  0.0, 0.0, 1.0
-    };
-
-    glMultMatrixf(matriz_reflexao);
-}
-
-void reflexao_horizontal() {
-    GLfloat matriz_reflexao[16] = {
-        -1.0,  0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
-        0.0,  0.0, 1.0, 0.0,
-        0.0,  0.0, 0.0, 1.0
-    };
-
-    glMultMatrixf(matriz_reflexao);
-}
-
-void reflexao_diagonal() {
-    GLfloat matriz_reflexao[16] = {
-        -1.0, 0.0, 0.0, 0.0,
-        0.0, -1.0, 0.0, 0.0,
-        0.0,  0.0, 1.0, 0.0,
-        0.0,  0.0, 0.0, 1.0
-    };
-
-    glMultMatrixf(matriz_reflexao);
-}
-
-void cisalhamento_y(GLfloat c) {
-    GLfloat matriz_cisalhamento[16] = {
-        1.0, c, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
-        0.0,  0.0, 1.0, 0.0,
-        0.0,  0.0, 0.0, 1.0
-    };
-
-    glMultMatrixf(matriz_cisalhamento);
+    multMatriz(matriz_escalar, triangulo, 3);
 }
 
 void cisalhamento_x(GLfloat c) {
-    GLfloat matriz_cisalhamento[16] = {
-        1.0, 0.0, 0.0, 0.0,
-        c, 1.0, 0.0, 0.0,
-        0.0,  0.0, 1.0, 0.0,
-        0.0,  0.0, 0.0, 1.0
+    GLfloat matriz_cisalhamento[3][3] = {
+        {1.0, c, 0.0},
+        {0.0, 1.0, 0.0},
+        {0.0, 0.0, 1.0}
     };
+    multMatriz(matriz_cisalhamento, triangulo, 3);
+}
 
-    glMultMatrixf(matriz_cisalhamento);
+void cisalhamento_y(GLfloat c) {
+    GLfloat matriz_cisalhamento[3][3] = {
+        {1.0, 0.0, 0.0},
+        {c, 1.0, 0.0},
+        {0.0, 0.0, 1.0}
+    };
+    multMatriz(matriz_cisalhamento, triangulo, 3);
+}
+
+void reflexao_vertical() {
+    GLfloat matriz_reflexao[3][3] = {
+        {1.0, 0.0, 0.0},
+        {0.0, -1.0, 0.0},
+        {0.0, 0.0, 1.0}
+    };
+    multMatriz(matriz_reflexao, triangulo, 3);
+}
+
+void reflexao_horizontal() {
+    GLfloat matriz_reflexao[3][3] = {
+        {-1.0, 0.0, 0.0},
+        {0.0, 1.0, 0.0},
+        {0.0, 0.0, 1.0}
+    };
+    multMatriz(matriz_reflexao, triangulo, 3);
+}
+
+void reflexao_diagonal() {
+    GLfloat matriz_reflexao[3][3] = {
+        {-1.0, 0.0, 0.0},
+        {0.0, -1.0, 0.0},
+        {0.0, 0.0, 1.0}
+    };
+    multMatriz(matriz_reflexao, triangulo, 3);
 }
--- transformacoes.h
+++ transformacoes.h
@@ -4,13 +4,14 @@
 #include <GL/glut.h>
 #include <math.h>
 
+void multMatriz(GLfloat matriz[3][3], Ponto* pontos, int num_pontos);
 void transladar(GLfloat tx, GLfloat ty);
 void rotacionar(GLfloat angulo);
-void escalar(GLfloat x, GLfloat y);
+void escalar(GLfloat sx, GLfloat sy);
+void cisalhamento_x(GLfloat c);
+void cisalhamento_y(GLfloat c);
 void reflexao_vertical();
 void reflexao_horizontal();
 void reflexao_diagonal();
-void cisalhamento_y(GLfloat c);
-void cisalhamento_x(GLfloat c);
 
 #endif
